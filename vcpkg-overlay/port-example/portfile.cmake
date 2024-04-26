ecd016a337d759e806c8b7a630677478c8f131d0
zhenkai.sun <zhenkai.sun@qq.com>
2024-04-26 22:56:01 +0800

    fix overlay port

--- vcpkg-overlay/port-example/portfile.cmake
+++ vcpkg-overlay/port-example/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_git(
         OUT_SOURCE_PATH SOURCE_PATH
         URL https://github.com/sunzhenkai/vcpkg-port-example.git
-        REF "5925c243e53de6faa9ba84798581c5a483c6bde6"
+        REF "31779a3466159980b483c710ec64cc73e571199a"
         HEAD_REF main
 )
 
@@ -14,7 +14,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "port-example")
 
-#file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
